17162e48e249dd44da5ff0424f4edac046d699c7
GussHdez <74477717+GussHdez@users.noreply.github.com>
2021-01-05 18:42:15 -0600

    Actualizacion para que corran los carousels despues

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,21 +1,25 @@
-$('#cardCarousel').carousel({
-  interval: 10000
-})
+$(document).ready(function() {
 
-$('.cardCarouselInner').each(function(){
-    var minPerSlide = 3;
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<minPerSlide;i++) {
-        next=next.next();
-        if (!next.length) {
-        	next = $(this).siblings(':first');
-      	}
-        
-        next.children(':first-child').clone().appendTo($(this));
+  $('#cardCarousel').carousel({
+    interval: 10000
+  })
+
+  $('.cardCarouselInner').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
       }
+      next.children(':first-child').clone().appendTo($(this));
+
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
 });
