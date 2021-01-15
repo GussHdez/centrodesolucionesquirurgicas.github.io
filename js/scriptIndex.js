77f46c43e606ff1c3dc292f3273d6a8d6589d615
GussHdez <74477717+GussHdez@users.noreply.github.com>
2021-01-14 20:45:26 -0600

    version2

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,25 +1,41 @@
 $(document).ready(function() {
+    
+    $(window).resize(function() {
+    console.log($(window).width());
+        if($('body').innerWidth() < 756) {
+       
+             $('#cardCarousel').carousel({
+                interval: 5000
+              })
 
-  $('#cardCarousel').carousel({
-    interval: 10000
-  })
+             $('.cardCarouselInner').each(function(){
+                  var minPerSlide = 3;
+                  var next = $(this).next();
+                  if (!next.length) {
+                  next = $(this).siblings(':first');
+                  }
+                  next.children(':first-child').clone().appendTo($(this));
 
-  $('.cardCarouselInner').each(function(){
-      var minPerSlide = 3;
-      var next = $(this).next();
-      if (!next.length) {
-      next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
+                  for (var i=0;i<minPerSlide;i++) {
+                      next=next.next();
+                      if (!next.length) {
+                        next = $(this).siblings(':first');
+                      }
 
-      for (var i=0;i<minPerSlide;i++) {
-          next=next.next();
-          if (!next.length) {
-            next = $(this).siblings(':first');
-          }
+                      next.children(':first-child').clone().appendTo($(this));
+                    }
+                });
+    
+            
+        } else {
+            
+            $('#cardCarousel').carousel({
+                interval: false
+              })
 
-          next.children(':first-child').clone().appendTo($(this));
-        }
-  });
+             
+        } 
+        
+    }).resize();
   
 });
