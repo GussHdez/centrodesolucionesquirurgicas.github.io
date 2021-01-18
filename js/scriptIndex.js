deddc50dea6dbe5529994c395783c38afbb5c5d9
GussHdez <74477717+GussHdez@users.noreply.github.com>
2021-01-17 18:14:27 -0600

    updated script version2

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -33,7 +33,23 @@ $(document).ready(function() {
                 interval: false
               })
 
-             
+            $('.cardCarouselInner').each(function(){
+                  var minPerSlide = 3;
+                  var next = $(this).next();
+                  if (!next.length) {
+                  next = $(this).siblings(':first');
+                  }
+                  next.children(':first-child').clone().appendTo($(this));
+
+                  for (var i=0;i<minPerSlide;i++) {
+                      next=next.next();
+                      if (!next.length) {
+                        next = $(this).siblings(':first');
+                      }
+
+                      next.children(':first-child').clone().appendTo($(this));
+                    }
+                });
         } 
         
     }).resize();
